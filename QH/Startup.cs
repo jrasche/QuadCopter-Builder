4f22d4aa05c7625c24da21956b606186c5d2478b
Jim Rasche <j.rasche@kunzleigh.com>
2016-08-31 14:04:51 -0400

    Add project files.

---
+++ QH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QH.Startup))]
+namespace QH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
